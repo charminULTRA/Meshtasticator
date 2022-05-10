984aaf7334f2358eadf01a37a9b916b69fddc9aa
GUVWAF <thijs@havinga.eu>
2022-05-10 22:04:41 +0200

    CW detail changes

--- lib/mac.py
+++ lib/mac.py
@@ -10,7 +10,7 @@ def setTransmitDelay(node, packet):  # from RadioLibInterface::setTransmitDelay
         if p.seq == packet.seq and p.rssiAtN[node.nodeid] != 0 and p.receivedAtN[node.nodeid] == True: 
             # verboseprint('At time', round(self.env.now, 3), 'pick delay with RSSI of node', self.nodeid, 'is', p.rssiAtN[self.nodeid])
             return getTxDelayMsecWeighted(node, p.rssiAtN[node.nodeid])  # weigthed waiting based on RSSI
-    return getTxDelayMsec()
+    return getTxDelayMsec(packet)
 
 
 def getTxDelayMsecWeighted(node, rssi):  # from RadioInterface::getTxDelayMsecWeighted
@@ -28,12 +28,13 @@ def getTxDelayMsecWeighted(node, rssi):  # from RadioInterface::getTxDelayMsecWe
     CWmax = 7
     CWsize = int((snr - SNR_MIN) * (CWmax - CWmin) / (SNR_MAX - SNR_MIN) + CWmin)
     if node.isRouter == True:
-        if CWsize == 1:
-            CW = random.randint(0, 2*CWsize-1)
-        else:
-            CW = random.randint(0, 4*CWsize-5)
+        # if CWsize == 1:
+        CW = random.randint(0, 2*CWsize-1)
+        # else:
+        #    CW = random.randint(0, 4*CWsize-5)
     else:
-        CW = random.randint(4*CWsize-4, 4*CWsize-1)
+        CW = random.randint(0, 4*CWsize-1)
+        # CW = random.randint(4*CWsize-4, 4*CWsize-1)
 
     # if node.isRouter == True:
     #     CWmin = 1
@@ -48,16 +49,17 @@ def getTxDelayMsecWeighted(node, rssi):  # from RadioInterface::getTxDelayMsecWe
     return CW * min_Tx_wait_msec
 
 
-def getTxDelayMsec():  # from RadioInterface::getTxDelayMsec
-    CWsize = 2
+def getTxDelayMsec(packet):  # from RadioInterface::getTxDelayMsec
+    CWsize = 5-packet.retransmissions
     CW = random.randint(0, 2**CWsize-1)
     verboseprint('Picked CW', CW)
     return CW * min_Tx_wait_msec
 
 
 def getRetransmissionMsec(packet):  # from RadioInterface::getRetransmissionMsec
+    CWsize = 5-packet.retransmissions
     packetAirtime = int(airtime(conf.SFMODEM[conf.MODEM], conf.CRMODEM[conf.MODEM], packet.packetlen, conf.BWMODEM[conf.MODEM]))
-    return 2*packetAirtime + (2**2-1) * min_Tx_wait_msec + (4*7-1) * min_Tx_wait_msec
+    return 2*packetAirtime + (2**CWsize-1) * min_Tx_wait_msec + (4*7-1) * min_Tx_wait_msec
 
 
 if VERBOSE:
